29ef5d97897c68b71bcfe36fe7a31245a51de177
Harry Freitas da Cruz <Harry.FreitasDaCruz@hpi.de>
2019-06-22 15:11:07 +0200

    Added files for testing jobs / plotting etc.

---
+++ test.py
@@ -0,0 +1,60 @@
+import morpher
+import morpher.config as config
+from morpher.jobs import *
+from morpher.metrics import *
+from morpher.plots import *
+import matplotlib.pyplot as plt, mpld3
+
+target = "AKI"
+
+data = Load().execute(source=config.FILE, filename="full.csv")
+
+data = Impute().execute(data, imputation_method=config.DEFAULT)
+
+train, test = Split().execute(data, test_size=0.3)
+
+#models = Train().execute(train, target=target, algorithms=[config.DECISION_TREE, config.RANDOM_FOREST, config.GRADIENT_BOOSTING_DECISION_TREE, config.MULTILAYER_PERCEPTRON])
+#models = Train(None).execute(train, target=target, algorithms=[config.DECISION_TREE, config.RANDOM_FOREST, config.GRADIENT_BOOSTING_TREE, config.LOGISTIC_REGRESSION, config.MULTILAYER_PERCEPTRON])
+#models = Train(None).execute(train, target=target, algorithms=[config.DECISION_TREE, config.RANDOM_FOREST])
+#models = Train().execute(train, target=target, algorithms=[config.LOGISTIC_REGRESSION])
+
+models = Train().execute(train, target=target, algorithms=[config.LOGISTIC_REGRESSION, config.RANDOM_FOREST, config.GRADIENT_BOOSTING_DECISION_TREE, config.MULTILAYER_PERCEPTRON])
+
+results = Evaluate().execute(test, target=target, models=models)
+
+plot_dc(results)
+
+plt.show()
+
+#plot_roc(results)
+
+
+#print (mpld3.fig_to_dict(fig=plt.gcf()))
+
+# #for clf_name in models:
+# #	results[clf_name]["clf"] = models[clf_name].clf
+
+# #plot_cc(models, train, test, target)
+# mpld3.show()
+
+# #print (mpld3.fig_to_dict(fig=plt.gcf()))
+
+# #chart.serve()
+
+for alg in results:
+	print("Printing metrics for %s" % alg)
+	print (get_clinical_usefulness_metrics(get_discrimination_metrics(**results[alg]), tr=0.03))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
