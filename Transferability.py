4386de614ad9a0c884db33a06365093970018cd7
Margaux <margaux.gatrio@student.hpi.de>
2019-07-02 19:20:30 +0200

    Added calibration metric and new Transferability score
    - Added calibration slope and intercepetion to metrics
    - First version of the new Transferability score using R2 added

---
+++ Transferability.py
@@ -0,0 +1,69 @@
+import morpher
+import morpher.config as config
+from morpher.jobs import *
+from morpher.metrics import *
+from morpher.plots import *
+import matplotlib.pyplot as plt, mpld3
+import pathlib
+
+target = "STROKE"
+
+data = Load().execute(source=config.FILE, filename='stroke_preprocessed_imputed_0.csv')
+
+data = Impute().execute(data, imputation_method=config.DEFAULT)
+
+train, test = Split().execute(data, test_size=0.3)
+
+models = Train().execute(train, target=target, algorithms=[
+	config.LOGISTIC_REGRESSION])#, config.RANDOM_FOREST, config.GRADIENT_BOOSTING_DECISION_TREE, config.MULTILAYER_PERCEPTRON])
+
+results = Evaluate().execute(test, target=target, models=models)
+
+for alg in results:
+	auc_org = int(get_discrimination_metrics(**results[alg])['auc']*100)
+
+
+path = pathlib.Path('./Stroke_data_test')
+auc = []
+
+for entry in path.iterdir():
+
+	test = Load().execute(source=config.FILE, filename=entry)
+
+	test = Impute().execute(test, imputation_method=config.DEFAULT)
+
+	results = Evaluate().execute(test, target=target, models=models)
+
+	for alg in results:
+
+		test = get_discrimination_metrics(**results[alg])
+
+		auc.append(int(test['auc']*100))
+
+		print('Original AUC:', auc_org)
+		print('AUCs:', auc)
+
+dis_to_org = 0
+dis_to_mean = 0
+r2 = 0
+
+mean_auc = (sum(auc)+auc_org)/(len(auc)+1)
+
+print('Mean AUC:', mean_auc)
+
+for i in auc:
+	dis_to_org += (i - auc_org)**2
+	dis_to_mean += (i - mean_auc)**2
+	r2 = 1 - (dis_to_org / dis_to_mean)
+
+print('Distance to original dataset:', dis_to_org)
+print('Distance to AUC Mean:', dis_to_mean)
+print('R2:', r2)
+
+
+
+#print("Printing metrics for %s" % alg)
+	#print(get_clinical_usefulness_metrics(get_discrimination_metrics(**results[alg]), tr=0.03))
+	#print(get_calibration_metrics(**results[alg]))
+
+
--- morpher/metrics.py
+++ morpher/metrics.py
@@ -5,6 +5,9 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, roc_curve, brier_score_loss, explained_variance_score, mean_squared_error, mean_absolute_error
 from collections import defaultdict
+from sklearn.calibration import calibration_curve
+from scipy.stats import linregress
+
 
 def get_discrimination_metrics(y_true, y_pred, y_probs, label="1.0"):
     '''
@@ -27,6 +30,26 @@ def get_discrimination_metrics(y_true, y_pred, y_probs, label="1.0"):
         results['dor'] = 0.0
 
     return dict(results)
+
+
+def get_calibration_metrics(y_true, y_pred, y_probs):
+
+    '''
+     Returns calibration metrics of the prediction results in a dictionary:
+    '''
+
+    results = defaultdict(lambda: {})
+
+    fraction_of_positives, mean_predicted_value = calibration_curve(y_true, y_probs, n_bins=10)
+
+    slope, intercept, r_value, p_value, std_err = linregress(fraction_of_positives, mean_predicted_value)
+
+    results['calibration_slope'] = slope
+
+    results['calibration_intercept'] = intercept
+
+    return dict(results)
+
 
 def get_clinical_usefulness_metrics(discrimination_metrics, tr=0.7):
     '''
