2859eee557390e06aecc5d7ea570766e11a42b2a
Harry Freitas da Cruz <Harry.FreitasDaCruz@hpi.de>
2019-07-31 21:38:04 +0200

    Fixed get_task method in MORPHER Job

--- morpher/jobs/morpher_job.py
+++ morpher/jobs/morpher_job.py
@@ -56,7 +56,7 @@ class MorpherJob(Job):
         '''
         Gets the parameters defined for the given task_id.
         '''
-        stmt = "SELECT id, status, pipeline_id, parameters, fastq_readcount, created_at, \"user\" FROM worker.\"TASKS\" WHERE id = :task_id";
+        stmt = "SELECT id, status, pipeline_id, parameters, fastq_readcount, created_at, user FROM worker.\"TASKS\" WHERE id = :task_id";
         
         try:
             return self.execute_select_stmt(stmt, {"task_id" : self.task_id}).first()
