2d443d5bf4bde6b8ef73d8e5d566fd077a3dc6ed
Margaux <margaux.gatrio@student.hpi.de>
2019-08-13 18:31:15 +0200

    Fixed Indentation

--- morpher/jobs/Train.py
+++ morpher/jobs/Train.py
@@ -78,35 +78,35 @@ class Train(MorpherJob):
     def execute(self, data, target,optimize=None, **kwargs):
         try:
 
-          if not data.empty:
+            if not data.empty:
 
-            ''' if split_data was called beforehand, data contains a subset of the original available data '''
-            labels = data[target]
-            features = data.drop(target, axis=1)
-            params = {}
-            algorithms = kwargs.get("algorithms")
-            kwarg_not_empty(algorithms,"algorithms")
+                ''' if split_data was called beforehand, data contains a subset of the original available data '''
+                labels = data[target]
+                features = data.drop(target, axis=1)
+                params = {}
+                algorithms = kwargs.get("algorithms")
+                kwarg_not_empty(algorithms,"algorithms")
 
-            trained_models = {}
+                trained_models = {}
 
-            for algorithm in algorithms:
-	        if optimize:                   		
-                     clf = eval("{algorithm}(optimize='yes')".format(algorithm=algorithm)) #instantiate the algorithm in runtime gridsearch
-            	else:
-                     clf = eval("{algorithm}()".format(algorithm=algorithm)) #instantiate the algorithm in runtime
+                for algorithm in algorithms:
+                    if optimize:
+                         clf = eval("{algorithm}(optimize='yes')".format(algorithm=algorithm)) #instantiate the algorithm in runtime gridsearch
+                    else:
+                         clf = eval("{algorithm}()".format(algorithm=algorithm)) #instantiate the algorithm in runtime
 
-                clf.fit(features, labels)
-                trained_models[algorithm] = clf
+                    clf.fit(features, labels)
+                    trained_models[algorithm] = clf
 
-            if kwargs.get("persist") is True:
-                params["target"] = target
-                params["features"] = features.columns
-                self.persist(trained_models, params)
+                if kwargs.get("persist") is True:
+                    params["target"] = target
+                    params["features"] = features.columns
+                    self.persist(trained_models, params)
 
-            return trained_models
+                return trained_models
 
-          else:
-            raise AttributeError("No data provided")        
+            else:
+                raise AttributeError("No data provided")
 
         except Exception as e:
             print(traceback.format_exc())
