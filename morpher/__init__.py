9846a92852b7f4205e7970b13f162f2a05c13eb5
Harry Freitas da Cruz <Harry.FreitasDaCruz@hpi.de>
2019-11-03 14:30:25 +0100

    Setting random seed to be picked up by frameworks, such as sklearn

--- morpher/__init__.py
+++ morpher/__init__.py
@@ -1,5 +1,9 @@
 import logging
 import morpher.config
 import morpher.jobs
+import numpy as np
 
-logging.basicConfig(filename='development.log',level=logging.DEBUG, format='%(asctime)s : %(levelname)s : %(message)s')
+#Setting random seed to be picked up by frameworks, such as sklearn
+np.random.seed(1729)
+
+logging.basicConfig(filename='morpher.log',level=logging.DEBUG, format='%(asctime)s : %(levelname)s : %(message)s')
