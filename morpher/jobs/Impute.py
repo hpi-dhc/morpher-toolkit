7b9c97bfa5e800ec2ef981fb494f1aa3422ad1c2
Harry Freitas da Cruz <Harry.FreitasDaCruz@hpi.de>
2019-12-04 16:29:02 +0100

    Impute and Scale return the fitted object to be used later on on the test set

--- morpher/jobs/Impute.py
+++ morpher/jobs/Impute.py
@@ -3,7 +3,7 @@ import traceback
 import logging
 import pandas as pd
 import morpher.config as config
-from morpher.imputers import *
+#from morpher.imputers import *
 from morpher.config import imputers
 from morpher.jobs import MorpherJob
 from morpher.exceptions import kwarg_not_empty
@@ -17,23 +17,25 @@ class Impute(MorpherJob):
 
         df = pd.read_csv(filepath_or_buffer= filename)
         imputation_method = self.get_input_variables("imputation_method")
-        df = self.execute(df, imputation_method=imputation_method)
+        df, imputer = self.execute(df, imputation_method=imputation_method)
         self.add_output("filename", self.save_to_file(df))
         self.add_output("cohort_id", self.get_input("cohort_id"))
         self.add_output("user_id", self.get_input("user_id"))
         self.logger.info("Data imputed successfully.")
 
-    def execute(self, data, imputation_method=imputers.DEFAULT,**kwargs):
+    def execute(self, data, imputation_method=imputers.DEFAULT, imputer=None,**kwargs):
         try:
           
           if not data.empty:
 
-            imputer = globals()[imputation_method](**kwargs)
-
             ''' columns where all values are NaN get assigned 0, otherwise imputer will throw them away '''
             data.loc[:, data.isna().all()] = 0.0
 
-            imputed_df = pd.DataFrame(imputer.fit_transform(data))
+            if not imputer:
+                imputer = globals()[imputation_method](**kwargs)            
+                imputer.fit(data)
+
+            imputed_df = pd.DataFrame(imputer.transform(data))            
             imputed_df.columns = data.columns
             imputed_df.index = data.index
 
@@ -46,4 +48,4 @@ class Impute(MorpherJob):
           print(traceback.format_exc())  
           logging.error(traceback.format_exc())
 
-        return data       
+        return data, imputer
--- morpher/jobs/Scale.py
+++ morpher/jobs/Scale.py
@@ -14,28 +14,31 @@ class Scale(MorpherJob):
 
         filename = self.get_input("filename")
         target = self.get_input_variables("target")
-        transform_method = self.get_input_variables("transform_method")
+        scale_method = self.get_input_variables("scale_method")
 
         df = pd.read_csv(filepath_or_buffer= filename)
-        df = self.execute(df, target, transform_method=transform_method)
+        df, scaler = self.execute(df, target, scale_method=scale_method)
         self.add_output("filename", self.save_to_file(df))
         self.add_output("cohort_id", self.get_input("cohort_id"))
         self.add_output("user_id", self.get_input("user_id"))
         self.add_output("target", target)
-        self.logger.info("Data transformed successfully.")
+        self.logger.info("Data scaled successfully.")
 
-    def execute(self, data, target, transform_method=scalers.DEFAULT,**kwargs):
+    def execute(self, data, target, scale_method=scalers.DEFAULT, scaler=None,**kwargs):
         try:
           
           if not data.empty:
 
-            transform = globals()[transform_method](**kwargs)
             labels = data[target]
-            features = data.drop(target, axis=1)            
+            features = data.drop(target, axis=1)
             
+            if not scaler:
+                scaler = globals()[scale_method](**kwargs)
+                scaler.fit(features)      
+                     
             ''' We refrain from transforming the label, that's why we need to concat both frames '''
             df_labels = labels.to_frame()
-            df_features = pd.DataFrame(transform.fit_transform(features))
+            df_features = pd.DataFrame(scaler.transform(features))
             df_labels.reset_index(drop=True, inplace=True)
             df_features.reset_index(drop=True, inplace=True)
             transformed_df = pd.concat([df_labels,df_features], axis=1)
@@ -50,4 +53,4 @@ class Scale(MorpherJob):
           print(traceback.format_exc())  
           logging.error(traceback.format_exc())
 
-        return data       
+        return data,scaler
