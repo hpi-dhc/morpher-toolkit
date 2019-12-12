e450c7542b0741824d2304006f1fcf26b0067d60
Harry Freitas da Cruz <Harry.FreitasDaCruz@hpi.de>
2019-12-12 11:18:51 +0100

    Added function to provide description of the data

--- morpher/utils.py
+++ morpher/utils.py
@@ -2,6 +2,12 @@ import jsonpickle as jp
 import inspect
 import jsonpickle.ext.numpy as jsonpickle_numpy
 import time
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from scipy import stats
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import Normalizer
 
 jsonpickle_numpy.register_handlers()
 
@@ -41,6 +47,66 @@ def retrieve_name(var):
         names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
         if len(names) > 0:
             return names[0]
+
+def data_description_to_csv(data, target, path, print_summary=False):
+    
+    if data is None:
+        raise AttributeError("No data has been loaded.")
+ 
+    summary = (
+        "### Data description:\n"
+        "# Rows: {0}\n"
+        "# Columns: {1}\n"
+        "# Column names: {2}"
+        ).format(data.shape[0], data.shape[1], data.columns.values)
+
+    summary += ("\n### Descriptive Statistics for Features Used\n")
+    bool_cols = [col for col in data if data[col].dropna().value_counts().index.isin([0, 1]).all()]
+    data_description = data.describe(include='all')
+
+    pvalues = p_values(data, target)
+    pvalues[target] = .0 #p-value for the target is by definition 0   
+
+    # initialize frame header
+    header = ["Column","All(n={0})".format(data.shape[0]), "Missing" , "Missing(%)" , "P"]
+    stats = pd.DataFrame(columns=header)
+
+    for column in data.columns:
+        if column in bool_cols:
+            count = data.loc[data[column] == 1].shape[0]
+            stats.loc[len(stats)] = [column, "{0} ({1}%)".format(count, round((count / data[column].dropna().count()) * 100, 2)), data[column].isnull().sum(), round((data[column].isnull().sum() / data.shape[0]) * 100, 1), round(pvalues[column],3)]
+        else:
+            stats.loc[len(stats)] = [column, "{0}+/-{1}".format(round(data_description[column]["mean"],2),round(data_description[column]["std"],2)), data[column].isnull().sum(), round((data[column].isnull().sum() / data.shape[0])*100,1), round(pvalues[column],3)]
+ 
+    stats.to_csv(path,index=False, sep=";")
+    print(f"Saved description to {path}.")
+
+    if print_summary:
+    	print(summary)
+
+    return stats
+
+def p_values(data, target, print_summary=False):
+    '''
+    Prints statistical information such as p-values
+    '''
+    if data is None:
+        raise AttributeError("No data available, please load some data.")
+
+    '''
+    Initializes pipeline by imputing and normalizing data
+    '''    
+    labels = data[target]
+    features = data.drop(target, axis=1)
+    columns = features.columns.values    
+    X = pd.DataFrame(Normalizer().fit_transform(SimpleImputer(strategy='mean').fit_transform(features)),columns=columns )
+    y = labels
+    X = sm.add_constant(X)
+    est = sm.OLS(y,X).fit()
+    if print_summary:
+    	print(est.summary())
+    
+    return est.pvalues
 
 class Timer:    
     def __enter__(self):
