e1015764b2fbbd38ac25118d41478d602733043c
Tom Martensen <tom.martensen@student.hpi.de>
2020-01-28 10:56:59 +0100

    remove duplicated method

--- morpher/metrics.py
+++ morpher/metrics.py
@@ -35,21 +35,6 @@ def get_discrimination_metrics(y_true, y_pred, y_probs, label="True"):
     return dict(results)
 
 
-def get_calibration_metrics(y_true, y_pred, y_probs, n_bins=10):
-
-    '''
-     Returns calibration metrics of the prediction results in a dictionary:
-    '''
-
-    results = defaultdict(lambda: {})
-    fraction_of_positives, mean_predicted_value = calibration_curve(y_true, y_probs, n_bins=n_bins)
-    slope, intercept, r_value, p_value, std_err = linregress(fraction_of_positives, mean_predicted_value)
-    results['calibration_slope'] = slope
-    results['calibration_intercept'] = intercept
-
-    return dict(results)
-
-
 def get_clinical_usefulness_metrics(discrimination_metrics, tr=0.7):
     '''
     Returns clinical usefulness of the prediction results in a dictionary
@@ -96,8 +81,7 @@ def get_clinical_usefulness_metrics(discrimination_metrics, tr=0.7):
     return dict(results)
 
 
-def get_calibration_metrics(y_true, y_probs, n_bins=10):  # Why the redefinition?
-
+def get_calibration_metrics(y_true, y_probs, n_bins=10):
     '''
      Returns calibration metrics of the prediction results in a dictionary:
     '''
