4bdab8dbb6ce0265158c23fe9702d36759cabfde
Tom Martensen <tom.martensen@student.hpi.de>
2020-02-13 11:46:57 +0100

    add no-sampler to samplers

--- morpher/samplers.py
+++ morpher/samplers.py
@@ -8,13 +8,31 @@ from imblearn.over_sampling import (
 )
 from imblearn.under_sampling import ClusterCentroids, RandomUnderSampler
 
+
+class NoSampler(object):
+    __name__ = 'no sampler'
+
+    def __init__(self, **kwargs):
+        pass
+
+    def fit_resample(self, **kwargs):
+        pass
+
+    def __bool(self):
+        return False
+
+    def __repr__(self):
+        return self.__name__
+
+
 _options = {
     'SMOTE': SMOTE,
     'BORDERLINE': BorderlineSMOTE,
     'ADASYN': ADASYN,
     'RANDOM': RandomOverSampler,
     'URANDOM': RandomUnderSampler,
-    'CLUSTER': ClusterCentroids
+    'CLUSTER': ClusterCentroids,
+    'NOSAMPLER': NoSampler,
 }
 
 sampler_config = namedtuple('options', _options.keys())(**_options)
