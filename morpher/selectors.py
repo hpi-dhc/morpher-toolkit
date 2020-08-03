b8b6e4dac20e21c27188c3816f8491491c603f6a
Harry Cruz <harrycruz@gmail.com>
2020-08-03 13:26:38 +0000

    Added CV functionality to ElasticNet

--- morpher/selectors.py
+++ morpher/selectors.py
@@ -3,7 +3,7 @@ import skrebate
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, mutual_info_classif, f_classif
 from boruta import BorutaPy
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import ElasticNet, ElasticNetCV
 from sklearn.base import BaseEstimator
 
 class NoSelector(object):
@@ -85,15 +85,49 @@ class ElasticNetSelector(BaseEstimator):
     """
     __name__ = "Elastic Net"
 
-    def __init__(self, top=10, **kwargs):
+    def __init__(self, top=10, cross_validate=False, verbose=False, **kwargs):
         kwargs['normalize'] = kwargs.get('normalize') or True
         kwargs['alpha'] = kwargs.get('alpha') or 0.001
         self.classifier_ = ElasticNet(**kwargs)
         self.top_ = top
+        self.cross_validate_ = cross_validate
+        self.verbose = verbose
 
     def fit(self, X, y):
+        if self.cross_validate_:
+            cv_model = ElasticNetCV(
+                l1_ratio=[.1, .5, .7, .9, .95, .99, .995, 1],
+                eps=0.001,
+                n_alphas=100,
+                fit_intercept=True, 
+                normalize=True,
+                precompute='auto',
+                max_iter=2000,
+                tol=0.0001,
+                cv=5, 
+                copy_X=True,
+                verbose=0,
+                n_jobs=-1,
+                positive=False,
+                selection='cyclic'
+            )
+
+            cv_model.fit(X, y)
+
+            if self.verbose:
+                print('Optimal alpha: %.8f'%cv_model.alpha_)
+                print('Optimal l1_ratio: %.3f'%cv_model.l1_ratio_)
+                print('Number of iterations %d'%cv_model.n_iter_)
+
+            self.classifier_ = ElasticNet(
+                l1_ratio=cv_model.l1_ratio_,
+                alpha = cv_model.alpha_,
+                max_iter=cv_model.n_iter_,
+                fit_intercept=True,
+                normalize = True
+            )
+
         self.classifier_.fit(X, y)
-        print(self.classifier_.coef_)
     
     def transform(self, X):
         return X
